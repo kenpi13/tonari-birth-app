017f486def2b617c15b55dbe9e9f9b4a2c6bffcd
kenpi13 <kenkenpi0526@gmail.com>
2021-04-01 22:25:50 +0900

    fix image path

--- components/carourcel.component.js
+++ components/carourcel.component.js
@@ -14,28 +14,13 @@ export default function CarouselComponent() {
                     <img src="../static/002.jpg" />
                 </div>
                 <div>
-                    <img src="../static/003.jpg" />
+                    <img src="../static/001.jpg" />
+                </div>
+                <div>
+                    <img src="../static/002.jpg" />
                 </div>
             </Carousel>
         </div>
         </div>
     );
 }
-
-{/* <div className="content__sub__main__img">
-      <img className="w-vw" src="../static/001.jpg"/>
-    </div> */}
-{/* <div className="content__sub__img__wrap pt-1 flex">
-<div className="w-1/4">
-<img className="w-full" src="../static/001.jpg"></img>
-</div>
-<div className="w-1/4">
-<img className="w-full" src="../static/002.jpg"></img>
-</div>
-<div className="w-1/4">
-<img className="w-full" src="../static/003.JPG"></img>
-</div>
-<div className="w-1/4">
-<img className="w-full" src="../static/004.JPG"></img>
-</div>
-</div> */}
